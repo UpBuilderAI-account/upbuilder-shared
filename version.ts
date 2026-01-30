bd9d55e92f8ef2b0bc1b7950e9b5b0dfc9e2330a
tomas <marin73tomas@gmail.com>
2026-01-29 19:09:47 -0500

    update shared package

--- version.ts
+++ version.ts
@@ -13,7 +13,7 @@
  * If versions don't match, the backend rejects the connection with a
  * VERSION_OUTDATED error, prompting the user to close and reopen Figma
  */
-export const SHARED_VERSION = '2.1.138';
+export const SHARED_VERSION = '2.1.139';
 
 /**
  * Minimum supported version (optional - for backwards compatibility)
